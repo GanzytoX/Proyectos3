665757249625960e2d56afecf46181c2f3aed6c9
GanzytoX <gonzalo.gb11.ggb@gmail.com>
2023-10-09 20:04:49 -0600

    Update_Gonzalo

---
+++ curso.py
@@ -0,0 +1,3 @@
+texto = "Re zarpado"
+print(len(texto)) # len({variable}) devuelve el total de caracteres
+print(texto[0]) # {variable}[] devuelve el caracter en la posción indicada
--- interfaz.py
+++ interfaz.py
@@ -1,3 +1,2 @@
 import tkinter
 import customtkinter as ctk
-
