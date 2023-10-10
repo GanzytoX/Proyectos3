82d2c616c51f1069dd341886c5a58047b7758d93
GanzytoX <gonzalo.gb11.ggb@gmail.com>
2023-10-09 19:13:26 -0600

    Update_Gonzalo-2

---
+++ test.py
@@ -0,0 +1,20 @@
+import tkinter
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+root = customtkinter.CTk()  # create CTk window like you do with the Tk window
+root.title("Interfaz de la base de datos")
+root.geometry("400x240")
+
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=root, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER) # El ancla permite que esté en medio de donde le indiquemos, utiliza la ventana, no pantalla
+
+
+
+root.mainloop()
