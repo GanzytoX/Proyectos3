445a5cb5c0288356ed02bcbec91e13f0fcfb05d0
Ventthos <117321816+Ventthos@users.noreply.github.com>
2023-10-11 20:09:19 -0600

    Arreglando cosos victor

--- CRUD_producto.py
+++ CRUD_producto.py
@@ -1,20 +1,32 @@
 from AbstractCRUD import CRUD
 
 if __name__ != "__main__":
+
+
+    class Producto():
+        def __init__(self, nombre: str, descripcion: str, precio: float, imagen):
+            self.nombre = nombre
+            self.descripcion = descripcion
+            self.precio = precio
+            self.imagen = imagen
+
+    # Perdón por romper tu clase, asi que la arreglé
     class Crud_Producto(CRUD):
         def __init__(self, conection):
             self.__conection = conection
             self.__cursor = self.__conection.cursor()
 
-        def add(self, nombre, descripcion, precio, imagen):
+        def Create(self, product: Producto):
             script = ("INSERT INTO Producto(nombre, descripcion, precio, imagen)"
-                      f"VALUES ({nombre}, {descripcion}, {precio}, {imagen}")
+                      f"VALUES ({product.nombre}, {product.descripcion}, {product.precio}, {product.imagen}")
             self.__cursor.execute(script) #seria fetch si pidiera datos y execute si ingresa
             self.__conection.commit() # commit siempre que se modifique la tabla
 
-        def Update(self, id):
-            pass
+        def Update(self, product: Producto):
+            raise NotImplementedError()
+
         def Delete(self, id):
-            pass
-        def get(self, id):
-            pass
+            raise NotImplementedError()
+
+        def Read(self, id=None):
+            raise NotImplementedError()
