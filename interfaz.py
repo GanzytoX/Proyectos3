246b20eeb143936479b696b498c081b62f23a601
GanzytoX <gonzalo.gb11.ggb@gmail.com>
2023-10-13 17:44:33 -0600

    Update Interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -1,7 +1,19 @@
-# Importamos las librerías
+# Configuración de la ventana
+# root = CTk()
+# root.geometry("600x500")
+# root.title("Interfaz")
+# root.config()
 
+# # Cargar el logo
+# logo = PhotoImage(file = "img/logo.png")
+
+# root.call("wm", "iconphoto", root._w, logo)
+# root.mainloop()
+
+# Importamos las librerías
 from customtkinter import *
 from Widgets import *
+
 
 # Paleta de colores
 c_gris_claro = "#e7e7e5"
@@ -11,32 +23,16 @@ c_azul = "#185791"
 c_rojo_palido = "#c9636c"
 c_blanco = "#ffffff"
 
-""""
-# Configuración de la ventana
-root = CTk()
-root.geometry("600x500")
-root.title("Interfaz")
-root.config()
-
-# Cargar el logo
-logo = PhotoImage(file = "img/logo.png")
-
-root.call("wm", "iconphoto", root._w, logo)
-root.mainloop()
-"""""
-
-
-ventana = Window("Inicio de sesión", "1200x700")
-
+ventana = CTk()
+ventana.geometry("1200x700")
+ventana.title("Inicio de sesión")
+# ventana = Window("Inicio de sesión", "1200x700")
 ventana.setBackgroundImage("img/Inicio de sesión.png")
 
 frame = CTkFrame(ventana, bg_color="gray")
-frame2 = CTkFrame(ventana, bg_color="gray")
-widget = CTkBaseClass(ventana, bg_color="black")
+frame.place(x=ventana.winfo_width() /2, y=ventana.winfo_height() /10, anchor=CENTER)
 
-
-frame.grid(row=0, pady=(0,10), sticky=NSEW)
-frame2.grid(row=2, sticky=NSEW)
+button = CTkButton(ventana)
 
 
 ventana.mainloop()
