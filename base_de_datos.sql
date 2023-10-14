db0837bfeab44f78e9d4c44e972416675db0ac63
Ventthos <117321816+Ventthos@users.noreply.github.com>
2023-10-14 14:57:16 -0600

    Cambiando cosas del SQL
    Cosas mínimas para que el de BD no nos mate

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,97 +1,90 @@
--- CREATE DATABASE BaseDeDatos_Polleria;
--- USE BaseDeDatos_Polleria;
-
+-- CREATE DATABASE pollosexpress;
+-- USE pollosexpress;
+CREATE DATABASE pollosexpress;
+USE pollosexpress;
 -- SHOW TABLES;
 
-CREATE TABLE Pago (
-    id_Pago INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    metodo VARCHAR(50)
+CREATE TABLE pago (
+    id_pago INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre NVARCHAR(50)
 );
 
-CREATE TABLE Rol (
-    id_Rol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
+CREATE TABLE rol (
+    id_rol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre NVARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Empleado (
-    id_Empleado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50) NOT NULL,
+CREATE TABLE empleado (
+    id_empleado INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido_materno NVARCHAR(50) NOT NULL,
+    apellido_paterno NVARCHAR(50) NOT NULL,
     celular VARCHAR(11) NOT NULL,
     sueldo DOUBLE NOT NULL,
-    id_RolFK INT,
-    contraseña VARCHAR(20),
-    FOREIGN KEY (id_RolFK) REFERENCES Rol(id_Rol)
+    id_rol INT,
+    contraseña NVARCHAR(30),
+    FOREIGN KEY (id_rol) REFERENCES rol(id_Rol),
+    CONSTRAINT UQcelular UNIQUE(celular)
 );
-
-/*
-CREATE TABLE usuario(
-	id_usuario int AUTO_INCREMENT PRIMARY KEY,
-    id_empleado int,
-    
-);
-*/
 
 CREATE TABLE Venta (
     id_Venta INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     fecha_De_Venta DATE NOT NULL,
     total_De_Compra DOUBLE NOT NULL,
-    id_PagoFK INT,
-    id_EmpleadoFK INT,
-    FOREIGN KEY (id_PagoFK) REFERENCES Pago(id_Pago),
-    FOREIGN KEY (id_EmpleadoFK) REFERENCES Empleado(id_Empleado)
+    id_pago INT,
+    id_empleado INT,
+    FOREIGN KEY (id_pago) REFERENCES pago(id_Pago),
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id_Empleado)
 );
 
 
-/*SELECT * FROM Pago;*/
-
-CREATE TABLE Producto (
-    id_Producto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    descripcion VARCHAR(200) NOT NULL,
+CREATE TABLE producto (
+    id_producto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre NVARCHAR(100) NOT NULL,
+    descripcion NVARCHAR(200) NOT NULL,
     precio DOUBLE NOT NULL,
-    imagen VARCHAR(200) NOT NULL
+    imagen LONGTEXT NOT NULL
 );
 
-CREATE TABLE Gasto (
-    id_Gasto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE gasto (
+    id_gasto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     descripcion VARCHAR(150) NOT NULL,
     monto DOUBLE NOT NULL,
     fecha DATE NOT NULL,
-    id_EmpleadoFK INT,
-    FOREIGN KEY (id_EmpleadoFK) REFERENCES Empleado(id_Empleado)
+    id_empleado INT,
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
 );
 
-CREATE TABLE Cliente (
-    id_Cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE cliente (
+    id_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
     celular VARCHAR(11) NOT NULL,
     direccion VARCHAR(150) NOT NULL
 );
 
-CREATE TABLE Tipo_de_promocion (
-    id_Tipo_Promocion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE tipo_de_promocion (
+    id_tipo_promocion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     codigo VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Venta_Producto (
-    id_Venta_Producto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_VentaFK INT,
-    id_ProductoFK INT,
-    FOREIGN KEY (id_VentaFK) REFERENCES Venta(id_Venta),
-    FOREIGN KEY (id_ProductoFK) REFERENCES Producto(id_Producto)
+CREATE TABLE venta_producto (
+    id_venta_producto INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_venta INT,
+    id_producto INT,
+    FOREIGN KEY (id_venta) REFERENCES venta(id_venta),
+    FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
 );
 
-CREATE TABLE Promocion (
-    id_Promocion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_ProductoFK INT,
+CREATE TABLE promocion (
+    id_promocion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT,
     descripcion VARCHAR(200) NOT NULL,
     fecha_de_inicio DATE NOT NULL,
     fecha_de_finalizacion DATE,
-    id_Tipo_PromocionFK INT,
-    FOREIGN KEY (id_ProductoFK) REFERENCES Producto(id_Producto),
-    FOREIGN KEY (id_Tipo_PromocionFK) REFERENCES Tipo_de_promocion(id_Tipo_Promocion),
-    CONSTRAINT fecha_valida CHECK (fecha_de_inicio <= fecha_de_finalizacion) --VERIFICA QUE LA FECHA DE INICIO SEA MENOR A LA FECHA FINAL
+    id_tipo_promocion INT,
+    FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
+    FOREIGN KEY (id_tipo_promocion) REFERENCES tipo_de_promocion(id_tipo_promocion),
+    CONSTRAINT fecha_valida CHECK (fecha_de_inicio <= fecha_de_finalizacion)
 );
