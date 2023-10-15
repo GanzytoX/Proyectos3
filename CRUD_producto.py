168868f66d2d1a1d0f12fdaae70315299448199a
Ventthos <117321816+Ventthos@users.noreply.github.com>
2023-10-15 17:57:11 -0600

    CRUD de producto

--- CRUD_producto.py
+++ CRUD_producto.py
@@ -1,11 +1,16 @@
-#pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib
 from AbstractCRUD import CRUD
+from PIL import Image
+from io import BytesIO
+
 
 if __name__ != "__main__":
 
 
     class Producto():
-        def __init__(self, nombre: str, descripcion: str, precio: float, imagen: any):
+        def __init__(self, nombre: str, descripcion: str, precio: float, imagen: any, id: int = None ):
+            self.id = 0
+            if id is not None:
+                self.id = id
             self.nombre = nombre
             self.descripcion = descripcion
             self.precio = precio
@@ -20,14 +25,40 @@ if __name__ != "__main__":
         def Create(self, product):
             script = "INSERT INTO producto(nombre, descripcion, precio, imagen) VALUES (%s, %s, %s, %s)"
             datos_producto = (product.nombre, product.descripcion, product.precio, product.imagen)
-            self.__cursor.execute(script, datos_producto) #seria fetch si pidiera datos y execute si ingresa
+            self.__cursor.execute(script, datos_producto) #seria fetch si pidiera datos
             self.__conection.commit() # commit siempre que se modifique la tabla
 
         def Update(self, product: Producto):
-            raise NotImplementedError()
+            script = ("UPDATE producto "
+                      "SET nombre = %s, descripcion = %s, precio = %s, imagen = %s "
+                      "WHERE id_producto = %s")
+            datos_producto = (product.nombre, product.descripcion, product.precio, product.imagen, product.id)
+            self.__cursor.execute(script, datos_producto)
+            self.__conection.commit()
 
         def Delete(self, id):
-            raise NotImplementedError()
+            script = f"DELETE FROM producto WHERE id = {id}"
+            self.__cursor.execute(script)
+            self.__conection.commit()
 
         def Read(self, id=None):
-            raise NotImplementedError()
+            if id is None:
+                script = "SELECT * from producto"
+                self.__cursor.execute(script)
+                result = self.__cursor.fetchall()
+                productos = []
+                for resultado in result:
+                    imgBytes = BytesIO(resultado[4])
+                    img = Image.open(imgBytes)
+
+                    producto = Producto(resultado[1], resultado[2], resultado[3], img, resultado[0])
+                    productos.append(producto)
+                return productos
+            elif id is int:
+                script = f"SELECT * from producto WHERE id_empleado = {id}"
+                self.__cursor.execute(script)
+                resultado = self.__cursor.fetchone()
+                imgBytes = BytesIO(resultado[4])
+                img = Image.open(imgBytes)
+                producto = Producto(resultado[1], resultado[2], resultado[3], img, resultado[0])
+                return producto
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from CRUD_Usuario import CrudEmpleado, Empleado
 from CRUD_producto import CrudProducto, Producto
 import mysql.connector
+from PIL import Image, ImageTk
 
-import os
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
 
 if __name__ == "__main__":
     conection = mysql.connector.connect(
@@ -15,19 +12,21 @@ if __name__ == "__main__":
         database="pollosExpress"
     )
 
-    productManager = CrudProducto(conection)
-    with open('img/logo.png', 'rb') as archivo:
-        imagen = archivo.read()
+    #NO ME BORREN DE AQUI SON PRUEBAS QUE USARE
+
+    #
+    #with open('img/logo.png', 'rb') as archivo:
+        #imagen = archivo.read()
     
     #productManager.Create(Producto("Papitas", "Estan ricas", 25.48, imagen))
 
-    script = "SELECT * FROM producto WHERE id_producto = 1"
-    cursor = conection.cursor()
-    cursor.execute(script)
-    data = cursor.fetchone()
+    #script = "SELECT * FROM producto WHERE id_producto = 1"
+    #cursor = conection.cursor()
+    #cursor.execute(script)
+    #data = cursor.fetchone()
 
-    with open("filename.png", 'wb') as file:
-        file.write(data[4])
+    #with open("filename.png", 'wb') as file:
+        #file.write(data[4])
 
     #userManager = CrudEmpleado(conection)
     #userManager.Create(Empleado("Dios", "Si", "quiza", "8788", 25000, 1, "si"))
@@ -36,5 +35,16 @@ if __name__ == "__main__":
     #userManager.Update(empleadito)
     #gestorProducto = Crud_Producto(conection)
     #gestorProducto.add("si", "a veces", 200, "img/mimikiu.png")
+
+    #productManager = CrudProducto(conection)
+    #with open('img/logo.png', 'rb') as archivo:
+        #imagen = archivo.read()
+    #productManager.Update(Producto("Papitas", "Estan ricas", 26, imagen, 1))
+
+    productManager = CrudProducto(conection)
+    productos = productManager.Read()
+    productos[0].imagen.show()
+
+
 
 
