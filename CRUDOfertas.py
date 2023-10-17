62f3435f5ac4d411629ad5643d590c353302bb63
tartaman <70653340+tartaman@users.noreply.github.com>
2023-10-17 12:55:09 -0600

    xd

---
+++ CRUDOfertas.py
@@ -0,0 +1,9 @@
+import mysql.connector.errors
+from AbstractCRUD import CRUD
+
+class Oferta():
+    def __init__(self, descripcion, fechaInicio, fechaFinal, ):
+        pass
+
+class CRUDOfertas(CRUD):
+    pass
