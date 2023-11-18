0cec1ccc5687ef0ddce64e0cca3e3b996909bc1a
Ventthos <117321816+Ventthos@users.noreply.github.com>
2023-11-18 12:28:15 -0600

    Avance de los widgets de pagar

--- Interfaces/PagoInterface.py
+++ Interfaces/PagoInterface.py
@@ -15,6 +15,7 @@ class Pagos(Toplevel):
         self.geometry("900x600")
         self.columnconfigure(0, weight=2)
         self.columnconfigure(1, weight=4)
+        self.rowconfigure(0, weight=10)
 
         # Frame del lado izquierdo
         self.__frame_izquierdo = Frame(self)
@@ -38,7 +39,7 @@ class Pagos(Toplevel):
         # Poner el label del total
         self.__TotalLabelCant =Label(frameTotal)
         self.__TotalLabelCant.grid(column=1, row=0, padx=10)
-        self.__calcularTotal()
+        total = self.__calcularTotal()
 
         # Button de cancelar venta (aunque simplemente se podría cerrar la ventana y queda)
         self.__button_cancelar = Button(self.__frame_izquierdo, text="Cancelar",
@@ -46,18 +47,46 @@ class Pagos(Toplevel):
         self.__button_cancelar.pack(side=tkinter.LEFT)
 
         # Frame del lado izquierdo
-        self.__frameDerecho = Frame(self, background="e7e7e5")
+        self.__frameDerecho = Frame(self, background="#e7e7e5")
         self.__frameDerecho.grid(column=1, row=0, sticky="NSEW", pady=20)
         self.__frameDerecho.columnconfigure(0, weight=3)
         self.__frameDerecho.columnconfigure(1, weight=5)
 
         # Frame de botones para pagar con efectivo o tarjeta
         frame_formas = Frame(self.__frameDerecho, background="white")
-        frame_formas.grid(column=0, row=0, sticky="EW")
+        frame_formas.grid(column=0, row=0, sticky="EW", padx=10)
 
         # Boton para efectivo
-        self.__buton_efectivo = Button(frame_formas, background="#185791", text="Efectivo")
-        self.__buton_efectivo.pack()
+        self.__buton_efectivo = Button(frame_formas, background="#185791", text="Efectivo", foreground="white")
+        self.__buton_efectivo.pack(padx=5)
+
+        # Frame para para efectivo
+        frame_efectivo = Frame(self.__frameDerecho, background="white")
+        frame_efectivo.grid(column=1, row=0, sticky="EW", padx=5)
+
+        # Elementos del frame de efectivo
+        texto_efectivo = Label(frame_efectivo, text="Efectivo")
+        texto_efectivo.pack(pady=5)
+
+        texto_total = Label(frame_efectivo, text="Total")
+        texto_total.pack(pady=5)
+
+        entry_total = Entry(frame_efectivo)
+        entry_total.insert(0, str(total))
+        entry_total.config(state="readonly")
+        entry_total.pack()
+
+        texto_recibido = Label(frame_efectivo, text="Recibido")
+        texto_recibido.pack(pady=5)
+
+        entry_recibido = Entry(frame_efectivo)
+        entry_recibido.pack()
+
+        texto_cambio = Label(frame_efectivo, text="Cambio")
+        texto_cambio.pack(pady=5)
+
+        entry_cambio = Entry(frame_efectivo, state="readonly")
+        entry_cambio.pack()
 
     def __copy_elements(self, elementos_list):
         for element in elementos_list:
