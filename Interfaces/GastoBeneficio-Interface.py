8af09cae2f714e43168b71aa42a7db8faf761a1a
tartaman <70653340+tartaman@users.noreply.github.com>
2023-11-19 18:50:40 -0600

    fechas completas

--- Interfaces/GastoBeneficio-Interface.py
+++ Interfaces/GastoBeneficio-Interface.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import mysql.connector
+from Utilities.FechaYMeses import *
 
 class GastoBeneficioInterface(Tk):
     def __init__(self):
@@ -28,7 +29,12 @@ class GastoBeneficioInterface(Tk):
         self.sueldoEmpleadoDiario = self.sueldoEmpleadoSemanal[0] / 7
 
         """Gastos como tal"""
-
+        #Necesito calcular conforme al dia que fueron hechos
+        fecha = fecha()
+        #Agarrar solo el dia
+        self.script = f"SELECT SUM(monto) FROM gasto WHERE fecha BETWEEN '{fecha.año}-{fecha.mes}-{fecha.dia} 00:00:00' AND '{fecha.año}-{fecha.mes}-{fecha.dia} 23:59:59'"
+        #Agarrando el mes completo
+        #self.script = f"SELECT SUM(monto) FROM gasto WHERE fecha BETWEEN '{fecha.año}-{fecha.mes}-01' AND '{fecha.año}-{fecha.mes}-{fecha.dias}'"
 
     def calcularGanacia(self):
         self.conection.reconnect()
---
+++ Utilities/FechaYMeses.py
@@ -0,0 +1,26 @@
+import time
+import datetime
+class fecha:
+    def __init__(self):
+        self.actual_time = time.localtime()
+        self.timeFormatted = time.strftime("%Y/%m/%d", self.actual_time)
+        self.timeFormatted = self.timeFormatted.split("/")
+        self.mes = int(self.timeFormatted[1])
+        self.año = int(self.timeFormatted[0])
+        self.dias = int
+        self.dia = int(self.timeFormatted[2])
+        self.calcularDias()
+        self.biciesto = False
+        self.semana = datetime.date(self.año, self.mes, self.dia).isocalendar().week
+    def calcularDias(self):
+        MesesTYU = [1, 3, 5, 7, 8, 10, 12]
+        MesesT = [4, 6, 9, 11]
+        if self.mes in MesesTYU:
+            self.dias = 31
+        elif self.mes in MesesT:
+            self.dias = 30
+        elif (self.año % 400 == 0 or self.año % 100 != 0) and self.año % 4 == 0:
+            self.dias = 29
+            self.biciesto = True
+        else:
+            self.dias = 28
