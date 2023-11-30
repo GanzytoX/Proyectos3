5e110768025c950b92501e2b1eb25c90953691fa
tartaman <70653340+tartaman@users.noreply.github.com>
2023-11-30 08:30:16 -0600

    uwu

--- Crud/AbstractCRUD.py
+++ Crud/AbstractCRUD.py
@@ -5,20 +5,15 @@ class CRUD(ABC):
     def __init__(self, conection):
         self._conection = conection
         self._cursor = self._conection.cursor()
-
-
     @abstractmethod
     def Create(self, object: object):
         raise NotImplementedError()
-
     @abstractmethod
     def Read(self, id=None, condition=None):
         raise NotImplementedError()
-
     @abstractmethod
     def Delete(self, id):
         raise NotImplementedError()
-
     @abstractmethod
     def Update(self, id: int , object: object):
         NotImplementedError()
